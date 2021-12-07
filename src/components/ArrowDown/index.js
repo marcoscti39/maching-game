12e3cc82fb55b755639cedb29a5fc957c9027d4d
Marcos <marcoscti39@gmail.com>
2021-12-07 16:39:42 -0300

    :dizzy: Add a transition to  the arrow
    :q

--- src/components/ArrowDown/index.js
+++ src/components/ArrowDown/index.js
@@ -1,9 +1,10 @@
 import "./style.css"
-function ArrowDown(){
+function ArrowDown(currentPlayer = 1){
     return /*html*/`
-    <div class="arrow-down">
-        <img src="images/ArrowDown.png">
-    </div>
+    <img class="arrow-down"
+     data-currentPlayer = "${currentPlayer}"
+     src="images/ArrowDown.png"
+     alt="indica vez do jogador">
     `
 }
 export default ArrowDown
